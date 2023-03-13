deb9b42459dce5e9fa6dd8736ba132b613ed23f9
mcclemily <4I78u8!!>
2023-03-12 21:06:27 -0700

    added platforms to ETL

---
+++ tables/platforms.changelog.mariadb.sql
@@ -0,0 +1,16 @@
+-- liquibase formatted sql
+-- changeset jesse:1
+create table word_of_mouth.platforms
+(
+    id      int           not null auto_increment,
+    platform   varchar(256)  null
+    constraint platforms_pk
+        primary key (id)
+);
+
+-- changeset jesse:2
+create table stage.platforms
+(
+    platform   varchar(256)  null,
+    summary varchar(4096) null
+);
